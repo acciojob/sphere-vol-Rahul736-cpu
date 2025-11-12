8345bf8c3c10553a4ce98cd4a710dcfc4db17061
Accio-1 <rahulwanjare291@gmail.com>
2025-11-12 16:13:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ function volume_sphere(event) {
 
     const radius = parseFloat(document.getElementById("radius").value);
 
+    if (isNaN(radius) || radius < 0) {
+        document.getElementById("volume").value = "NaN";
+        return;
+    }
+
     let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 
-    document.getElementById("volume").value = volume;
+    document.getElementById("volume").value = volume.toFixed(4);
 }
