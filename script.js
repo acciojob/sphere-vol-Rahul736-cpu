6ea2298dd2c3f877631d2e64e15d34e21a53a669
Accio-1 <rahulwanjare291@gmail.com>
2025-11-14 20:56:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function volume_sphere(event) {
-    event.preventDefault(); 
+function volume_sphere() {
+    const radiusInput = document.getElementById("radius").value;
 
-    const radius = parseFloat(document.getElementById("radius").value);
+    const r = parseFloat(radiusInput);
 
-    if (isNaN(radius) || radius < 0) {
+    if (isNaN(r) || r < 0) {
         document.getElementById("volume").value = "NaN";
         return;
     }
 
-    let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    const volume = (4 / 3) * Math.PI * Math.pow(r, 3);
 
     document.getElementById("volume").value = volume.toFixed(4);
 }
